3ab1157750deb31a55b7883a922e0df9abf6b85b
Dev SK <devbhagavan25@gmail.com>
2025-01-28 21:58:42 +0530

    minor changes

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from groq import prompts
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def home():
     content = prompts("Program for binary search")
     return content
