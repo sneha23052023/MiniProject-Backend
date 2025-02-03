143bc0dc83b2c968123965caffe49fde7ac37f5d
pranavsa7 <pranavsa2003@gmail.com>
2025-02-03 21:31:57 +0530

    separate prompt requests for comment and code added

--- server.py
+++ server.py
@@ -7,7 +7,15 @@ cors = CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
-    content = prompts("Program for binary search")
+    content = prompts('''//binary search
+    int binarySearch(int arr[], int low, int high, int x){
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if x is present at mid
+            if (arr[mid] == x)
+                return mid;
+                ''')
     return content
 
 
