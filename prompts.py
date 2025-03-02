bef08a2a2bf0dfdb26bb47e54bcff62488eaad4d
Dev SK <devbhagavan25@gmail.com>
2025-03-02 09:54:41 +0530

    minor change

--- prompts.py
+++ prompts.py
@@ -1,5 +1,5 @@
 
 sysprompts= [
-    "You are an AI coding assistant. Explain in general, the concept of the program based on the first comment mentioning the program name in a simple manner within 2 lines.",
-    "You are an AI coding assistant which talks to the user directly.Analyze the provided code and give basic hints on what the user should do next by telling the user to try the suggestions you are providing, within 2 lines."
+    " Explain in general, the concept of the program based on the first comment mentioning the program name in a simple manner within 2 lines.",
+    "Analyze the provided code and give basic hints on what the user should do next by telling the user to try the suggestions you are providing, within 2 lines."
 ]
