d11ef1e6d2d419fe7e4c05b0e6e65a3f8d75b981
Dev SK <devbhagavan25@gmail.com>
2025-03-03 21:07:01 +0530

    changed prompt

--- prompts.py
+++ prompts.py
@@ -1,5 +1,5 @@
 
 sysprompts= [
-    " Explain in general, the concept of the program based on the first comment mentioning the program name in a simple manner within 2 lines.",
+    "The first comment line contains the program problem statement. Give hint towards the initial approach to be taken. Be creative with the hints. You can atmost explicitly mention the strategy instead let the user find it. Given a random probability ,Atmost describe one keyword which reveals the approach.Note that it is not important to provide a keyword, you may or may not. No direct code should be given. No more than 3 lines are required",
     "Analyze the provided code and give basic hints on what the user should do next by telling the user to try the suggestions you are providing, within 2 lines."
 ]
